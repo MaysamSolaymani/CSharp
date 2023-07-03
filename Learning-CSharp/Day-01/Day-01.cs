78de2d7a1e6b085730d7a2603e6b2f5a820435dc
maysolivan <maysam.solaymani@gmail.com>
2023-07-03 13:23:43 +0330

    create folder day01

---
+++ Learning-CSharp/Day-01/Day-01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp
+{
+    class Day_01
+    {
+        public Day_01()
+        { 
+
+        }
+
+        public  void Numbers()
+        {
+            Console.WriteLine("subtraction 2 and 3 is: " + (2 + 3));   // addition(+)
+            Console.WriteLine("subtraction 3 and 1 is: " + (3 - 1));   // subtraction(-)
+            Console.WriteLine("multiplication 2 and 3 is: " + (2 * 3));   // multiplication(*)
+            Console.WriteLine("division 2 and 3 is: " + (3 / 2));   // division(/)
+            Console.WriteLine("exponential 2 and 3 is: " + (Math.Pow(3, 2)));  // exponential
+
+        }
+    }
+}
