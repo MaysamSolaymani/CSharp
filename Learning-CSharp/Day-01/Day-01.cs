639bf07865a736a9c63edaeaecf56a1712afc7f6
maysolivan <maysam.solaymani@gmail.com>
2023-07-03 13:40:42 +0330

    add code in day-01.cs

--- Learning-CSharp/Day-01/Day-01.cs
+++ Learning-CSharp/Day-01/Day-01.cs
@@ -15,12 +15,30 @@ namespace Learning_CSharp
 
         public  void Numbers()
         {
-            Console.WriteLine("subtraction 2 and 3 is: " + (2 + 3));   // addition(+)
-            Console.WriteLine("subtraction 3 and 1 is: " + (3 - 1));   // subtraction(-)
-            Console.WriteLine("multiplication 2 and 3 is: " + (2 * 3));   // multiplication(*)
-            Console.WriteLine("division 2 and 3 is: " + (3 / 2));   // division(/)
-            Console.WriteLine("exponential 2 and 3 is: " + (Math.Pow(3, 2)));  // exponential
-
+            Console.WriteLine("addition 2 and 3 is: " + (2 + 3));                 // addition(+)
+            Console.WriteLine("subtraction 3 and 1 is: " + (3 - 1));              // subtraction(-)
+            Console.WriteLine("multiplication 2 and 3 is: " + (2 * 3));           // multiplication(*)
+            Console.WriteLine("division 2 and 3 is: " + (3 / 2));                 // division(/)
+            Console.WriteLine("exponential 2 and 3 is: " + (Math.Pow(3, 2)));     // exponential
+            Console.WriteLine("-------------------------------------------------");
+            int num_a = 5;
+            int num_b = 7;
+            double num_c = 2.38;
+            double num_d = 3.79;
+            Console.WriteLine("addition 5 and 7 is: " + (num_a + num_b));
+            Console.WriteLine("addition 2.38 and 3.79 is: " + (num_c + num_d));
+            Console.WriteLine("addition 5 and 2.38 is: " + (num_a + num_c));
+            Console.WriteLine("----------------------");
+            Console.WriteLine(num_a++); //write before addition
+            Console.WriteLine(++num_a); //write after addition
+            Console.WriteLine("----------------------");
+            Console.WriteLine(num_c--); //write before subtraction
+            Console.WriteLine(--num_c); //write after subtraction
+            Console.WriteLine("----------------------");
+            Console.WriteLine(Math.PI);
+            Console.WriteLine(Math.E);
+            Console.WriteLine(Math.Sin(0));
+            Console.WriteLine(Math.Cos(0));
         }
     }
 }
