8f993b107b6ad1187c0c2e4824121fc2b622be5e
maysolivan <maysam.solaymani@gmail.com>
2023-07-03 12:29:42 +0330

    delete exe files and change in program.cs

--- Learning-CSharp/Program.cs
+++ Learning-CSharp/Program.cs
@@ -8,9 +8,22 @@ namespace Learning_CSharp
 {
     class Program
     {
+        public static void Numbers()
+        {
+            Console.WriteLine("subtraction 2 and 3 is: " + (2 + 3));   // addition(+)
+            Console.WriteLine("subtraction 3 and 1 is: " + (3 - 1));   // subtraction(-)
+            Console.WriteLine("multiplication 2 and 3 is: " + (2 * 3));   // multiplication(*)
+            Console.WriteLine("division 2 and 3 is: " + (3 / 2));   // division(/)
+            Console.WriteLine("exponential 2 and 3 is: " + (Math.Pow(3, 2)));  // exponential
+
+        }
         static void Main(string[] args)
         {
+            // write a text in console
             Console.Write("Maysam, Say Hello to the World!");
+            //call function
+            Numbers();
+            //wait for exit
             Console.ReadKey();
         }
     }
