461beea7b5fbd23c2391a09c19096c835165fecc
maysolivan <maysam.solaymani@gmail.com>
2023-07-08 12:12:26 +0330

    add new

---
+++ Learning/Tutorial3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp
+{
+
+        public class Vehicle  //parent
+        {
+            public string brand = "Ford";  // Vehicle field
+            public void honk()             // Vehicle method 
+            {
+                Console.WriteLine("Tuut, tuut!");
+            }
+
+            public Vehicle() { }
+        }
+
+        public class NewCar : Vehicle  // derived class (child)
+        {
+            public string modelName = "Mustang";  // Car field
+        }
+
+
+
+}
